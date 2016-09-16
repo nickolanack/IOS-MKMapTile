22113cc8a5ed5bdc702194da1f98b00c76f60be8
nickolanack <nickblackwell82@gmail.com>
2016-09-16 12:20:14 -0700

    added better caching and use parent tile (expand and crop) if missing

--- IOS-MKMapTile/Classes/ImageTileOverlay.m
+++ IOS-MKMapTile/Classes/ImageTileOverlay.m
@@ -32,7 +32,21 @@
         }
         
         if(err){
-            NSLog(@"err");
+            NSLog(@"Error unable to get tile");
+            
+            MKTileOverlayPath parent=[ImageTileOverlay ParentTilePathForPath:path];
+            
+            if(self.cache&&[self cachedTileExistsForPath:parent]){
+                
+                NSData *data=[ImageTileOverlay ParentTileImageSection:[self getCachedTileForPath:parent] ForPath:path];
+                if(data.length==0){
+                    NSLog(@"0");
+                }
+                result(data, nil);
+                return;
+            }
+            
+            
         }
         
         NSLog(@"%i, %i %i", path.x, path.y, path.z);
@@ -43,7 +57,50 @@
     
 }
 
++(NSData *)ParentTileImageSection:(NSData *)data ForPath:(MKTileOverlayPath)path{
+    
+    
+    int x=path.x%2;
+    int y=path.y%2;
+ 
 
+    UIImage *parent=[UIImage imageWithData:data];
+
+    
+    CGSize size=parent.size;
+    
+    float w=size.width/2;
+    float h=size.height/2;
+    
+    CGRect rect=CGRectMake(0+(w*x), 0+(h*y), w, h);
+    
+    CGImageRef imageRef = CGImageCreateWithImageInRect([parent CGImage], rect);
+    UIImage *cropped = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    
+    
+    
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
+    [cropped drawInRect:CGRectMake(0,0,w,h)];
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return UIImagePNGRepresentation(newImage);
+    
+}
+
+
++(MKTileOverlayPath)ParentTilePathForPath:(MKTileOverlayPath)path{
+    
+    MKTileOverlayPath parent;
+    parent.contentScaleFactor=path.contentScaleFactor;
+    parent.x=path.x/2;
+    parent.y=path.y/2;
+    parent.z=path.z-1;
+    
+    return parent;
+
+}
 
 -(bool)cachedTileExistsForPath:(MKTileOverlayPath)path{
     
