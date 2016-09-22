f38169bd04793f0abda80cd2838ba2d0a3b46237
Nicholas  Blackwell <nblackwe@Nicholass-iMac.local>
2016-09-22 00:27:45 -0700

    update pod

--- Example/Pods/Target Support Files/IOS-MKMapTile/IOS-MKMapTile-umbrella.h
+++ Example/Pods/Target Support Files/IOS-MKMapTile/IOS-MKMapTile-umbrella.h
@@ -1,5 +1,9 @@
 #import <UIKit/UIKit.h>
 
+#import "GridTileOverlay.h"
+#import "GridTileOverlayRenderer.h"
+#import "ImageTileOverlay.h"
+#import "ImageTileOverlayRenderer.h"
 
 FOUNDATION_EXPORT double IOS_MKMapTileVersionNumber;
 FOUNDATION_EXPORT const unsigned char IOS_MKMapTileVersionString[];
